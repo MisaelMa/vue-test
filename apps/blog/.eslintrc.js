bbcd30ae138cf99270a9e831a720604eafbb147e
MisaelMa <amisael.amir.misael@gmail.com>
2023-03-31 16:56:20 -0500

    feat(vercel): se corrgio el node

--- apps/blog/.eslintrc.js
+++ apps/blog/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    env: {
-      node: true,
-    },
+  "env": [
+    "jest"
+  ],
     extends: [
         'standard',
       'eslint:recommended',
--- apps/blog/src/components/PostList.vue
+++ apps/blog/src/components/PostList.vue
@@ -8,28 +8,35 @@ const props = defineProps({
 });
 
 const vTrimText = {
-  beforeMount: (el,binding) => {
+  beforeMount: (el, binding) => {
     console.log(binding.value);
-    const payload = binding.value
-    const {maxLength, last = '....'} = payload
+    const payload = binding.value;
+    const { maxLength, last = "...." } = payload;
     const text = el.innerText;
-    el.innerText = text.length > maxLength ? text.slice(0, maxLength) + last : text;
-  }
-}
+    el.innerText =
+      text.length > maxLength ? text.slice(0, maxLength) + last : text;
+  },
+};
 </script>
 <template>
-  <div class="h-56 max-w-sm p-1 overflow-hidden rounded shadow-lg border-neutral-800 ">
-    <div class="px-6 py-4">
-      <div v-trim-text="{maxLength:20,last:''}" class="mb-2 text-xl font-bold">
-        {{ title }}
+  <div
+    class="max-w-sm p-1 overflow-hidden rounded shadow-lg hover:text-white h-60 hover:bg-green-600 border-neutral-800"
+  >
+    <a :href="`#/post/${id}`">
+      <div class="px-6 py-4">
+        <div
+          v-trim-text="{ maxLength: 20, last: '' }"
+          class="mb-2 text-xl font-bold"
+        >
+          {{ title }}
+        </div>
+        <p v-trim-text="{ maxLength: 60 }" class="text-base">
+          {{ body }}
+        </p>
+        <p class="text-base">
+          {{ user.name }}
+        </p>
       </div>
-      <p v-trim-text="{maxLength:60,}" class="text-base text-gray-700">
-        {{ body }}
-      </p>
-      <p class="text-base text-gray-700">
-        {{ user.name }}
-      </p>
-        <a :href="`#/post/${id}`"><button type="button" class="btn btn-outline-success">Read More</button></a>
-    </div>
-   </div>
+    </a>
+  </div>
 </template>
