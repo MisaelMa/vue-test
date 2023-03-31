21f16d726d0215e456b1b44f7d220859080ae722
MisaelMa <amisael.amir.misael@gmail.com>
2023-03-31 18:17:45 -0500

    feat(test): se agrego vite test y coverage

---
+++ apps/blog/test/UseDetailsComponent.spec.ts
@@ -0,0 +1,15 @@
+import UserDetails from '../src/components/UserDetails.vue';
+import { mount } from '@vue/test-utils';
+
+describe('UserDetails', () => {
+  it('renderiza correctamente con los props proporcionados', () => {
+    const props = {
+      id: 1,
+      name: 'Neil',
+      email: 'neil@example.com'
+    };
+    const wrapper = mount(UserDetails, { props });
+    expect(wrapper.find('p.text-sm.font-medium.text-gray-900.truncate.dark\\:text-white').text()).toContain(props.name);
+    expect(wrapper.find('p.text-sm.text-gray-500.truncate.dark\\:text-gray-400').text()).toContain(props.email);
+  });
+});
